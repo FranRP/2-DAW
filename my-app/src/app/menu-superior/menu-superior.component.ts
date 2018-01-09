79501ac7425c0f18d060b26e54e9624b9c4e4dfd
FranRP <kirtashblog@gmail.com>
2018-01-09 14:15:56 +0100

    Directivas ngFor, ngIf y ngClass terminadas

--- my-app/src/app/menu-superior/menu-superior.component.ts
+++ my-app/src/app/menu-superior/menu-superior.component.ts
@@ -8,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class MenuSuperiorComponent implements OnInit {
 dato:string;
 clicks = 0;
-elementos:string[]=['Objeto1','Objeto2','Objeto3','Objeto4'];
+comprobacion:string;
+palabra:string;
+cantidad=5;
+elementos=[{nombre:'Objeto1',estado:'Activo'},{nombre:'Objeto2',estado:'Inactivo'},{nombre:'Objeto3',estado:'Activo'},{nombre:'Objeto4',estado:'Inactivo'}];
+
+texto:string='Si el valor del input tiene menos de 5 caracteres será verde, sino, será rojo';
 
 clickeame(){
   this.clicks++;
+}
+
+contador() {
+  this.palabra = this.comprobacion.length;
 }
   constructor() { }
 
