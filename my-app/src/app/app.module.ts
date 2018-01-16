821a9afda0cc1cf3584b0f444b1f96c5550de37c
FranRP <kirtashblog@gmail.com>
2018-01-16 08:38:15 +0100

    Actualizado con ajax

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 import { AppComponent } from './app.component';
@@ -14,6 +16,7 @@ import { FormsModule } from '@angular/forms'
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
--- my-app/src/app/menu-superior/menu-superior.component.ts
+++ my-app/src/app/menu-superior/menu-superior.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-menu-superior',
@@ -9,11 +10,21 @@ export class MenuSuperiorComponent implements OnInit {
 dato:string;
 clicks = 0;
 comprobacion:string;
-palabra:string;
+palabra:any;
 cantidad=5;
 elementos=[{nombre:'Objeto1',estado:'Activo'},{nombre:'Objeto2',estado:'Inactivo'},{nombre:'Objeto3',estado:'Activo'},{nombre:'Objeto4',estado:'Inactivo'}];
 
 texto:string='Si el valor del input tiene menos de 5 caracteres será verde, sino, será rojo';
+
+resultados:any[];
+
+constructor(private http: HttpClient) {}
+
+ngOnInit(): void {
+  this.http.get('https://restcountries.eu/rest/v2/name/aruba?fullText=true').subscribe(data => {
+    console.log(data);
+  });
+}
 
 clickeame(){
   this.clicks++;
@@ -22,9 +33,6 @@ clickeame(){
 contador() {
   this.palabra = this.comprobacion.length;
 }
-  constructor() { }
 
-  ngOnInit() {
-  }
 
 }
